f64619f276274c0a356086149051dc2ac0c1ad10
kaka7626 <hoang7626@gmail.com>
2023-06-20 23:37:28 +0700

    hàm range() có 3 đối số

---
+++ FOR_LOOPS-03.py
@@ -0,0 +1,6 @@
+#range có 3 đối số: số khởi đầu, số kết thúc (không bao gồm) và bước nhảy (có thể âm)
+for i in range(5, -1, -1):
+    print('Hiển thị: ' + str(i))
+
+# for i in range(5, 10, 2):
+#     print('Hiển thị: ' + str(i))
