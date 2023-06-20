478d5be62908fb179e75f79aa82e5d310ccff7cf
kaka7626 <hoang7626@gmail.com>
2023-06-20 23:36:44 +0700

    vòng lặp for có thể dùng break + continue

---
+++ FOR_LOOPS-01.py
@@ -0,0 +1,11 @@
+# for i in range(5):
+#     if i == 3:
+#         continue
+#     print('Hiển thị: ' + str(i) + ' lần')
+
+
+for i in range(5):
+    if i == 3:
+        break
+    print('Hiển thị: ' + str(i) + ' lần')
+#có thể sử dụng break, continue như vòng lặp while
